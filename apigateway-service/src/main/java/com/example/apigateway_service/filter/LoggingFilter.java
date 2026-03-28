8205cb82ceb2a5ed85f56f709e436e370832a8da
ajroot5685 <anjeong8577@naver.com>
2026-03-28 21:15:47 +0900

    1-1. 서비스 디스커버리, 유저 서비스, API Gateway 생성 및 연결 + 필터 설정

---
+++ apigateway-service/src/main/java/com/example/apigateway_service/filter/LoggingFilter.java
@@ -0,0 +1,50 @@
+package com.example.apigateway_service.filter;
+
+import com.example.apigateway_service.filter.GlobalFilter.Config;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+    public LoggingFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Logging Filter baseMessage: {}, {}", config.getBaseMessage(), request.getRemoteAddress());
+
+            if (config.isPreLogger()) {
+                log.info("Logging Filter Start: request uri -> {}", request.getURI().toString());
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    log.info("Logging Filter End: response code -> {}", response.getStatusCode());
+                }
+            }));
+        }, OrderedGatewayFilter.HIGHEST_PRECEDENCE);
+
+        return filter;
+    }
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
